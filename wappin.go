ece817323b6241260c0ea7ce6fab5c5b218d842b
Surahman <surahman.duang@gmail.com>
2020-01-02 14:24:38 +0700

    Send message to Wappin

---
+++ wappin.go
@@ -0,0 +1,83 @@
+package wappin
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+const SendHsmEndpoint  = "/v1/message/do-send-hsm"
+
+type Config struct {
+	ProjectId string
+	ClientSecret string
+	ClientKey string
+}
+
+// Handler interface for sending Whatsapp message
+type WaHandler interface {
+	SendWaMessage(reqMessage ReqWaMessage) (ResMessage, error)
+}
+
+type Sender struct {
+	Config Config
+	AccessToken AccessToken
+}
+
+// Request body for Whatsapp message
+type ReqWaMessage struct {
+	ClientId string
+	ProjectId string
+	Type string
+	RecipientNumber string
+	Params map[string]string
+}
+
+// Response body after sending message to Wappin
+type ResMessage struct {
+	MessageId string `json:"message_id"`
+	Status string `json:"status"`
+	Message string `json:"message"`
+	//Data string
+}
+
+// Create sender object
+func New(config Config) *Sender {
+	return &Sender{Config:config}
+}
+
+// Set authorization token
+func (s *Sender) setToken() {
+	accessToken, err := getAccessToken(s.Config.ClientSecret)
+
+	if err != nil {
+		panic(err)
+	}
+
+	s.AccessToken = accessToken
+}
+
+func (s *Sender) SendMessage(reqMsg interface{}) (res ResMessage, err error) {
+	s.setToken()
+
+	switch req := reqMsg.(type) {
+	case ReqWaMessage:
+		res, err = s.SendWaMessage(req)
+	default:
+		return ResMessage{}, errors.New("invalid request message format")
+	}
+
+	return res, err
+}
+
+// Send Whatsapp message
+func (s *Sender) SendWaMessage(req ReqWaMessage) (ResMessage, error) {
+	url := baseUrl + SendHsmEndpoint
+	res, err := client.R().SetBody(req).SetHeader("Authorization", s.AccessToken.Data.AccessToken).Post(url)
+	resMessage := ResMessage{}
+
+	if err := json.Unmarshal(res.Body(), &resMessage); err != nil {
+		return resMessage, err
+	}
+
+	return resMessage, err
+}
---
+++ wappin_test.go
@@ -0,0 +1,48 @@
+package wappin
+
+import (
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSendNotificationHSM(t *testing.T) {
+	httpmock.ActivateNonDefault(client.GetClient())
+	defer httpmock.DeactivateAndReset()
+	mockGetAccessToken()
+
+	fixture := `{ "message_id": "id-123", "status": "200", "message": "Success" }`
+	responder := httpmock.NewStringResponder(200, fixture)
+	fakeUrl := baseUrl + SendHsmEndpoint
+	httpmock.RegisterResponder("POST", fakeUrl, responder)
+
+	config := Config{
+		ProjectId:    "0123",
+		ClientSecret: "cs-key",
+		ClientKey:    "ck-key",
+	}
+	sender := New(config)
+	reqMsg := ReqWaMessage{
+		ClientId:        "123",
+		ProjectId:       "0123",
+		Type:            "template_name",
+		RecipientNumber: "089891234123",
+		Params: map[string]string{
+			"1": "John",
+			"2": "Depok",
+		},
+	}
+
+	res, _ := sender.SendMessage(reqMsg)
+
+	assert.Equal(t, "id-123", res.MessageId)
+	assert.Equal(t, "200", res.Status)
+	assert.Equal(t, "Success", res.Message)
+}
+
+func mockGetAccessToken() {
+	fixture := `{ "status": "200", "message": "Success", "data": { "access_token": "677b800f9b694f98bb9db6edb18336743a3f416cadff1953a59190f309220936", "expired_datetime": "2020-12-28 10:20:23", "token_type": "Bearer" } }`
+	responder := httpmock.NewStringResponder(200, fixture)
+	fakeUrl := baseUrl + TokenEndpoint
+	httpmock.RegisterResponder("POST", fakeUrl, responder)
+}
