a7afbb447b0e51ebbe6cd89faf813794d382c277
Surahman <surahman.duang@gmail.com>
2020-01-02 14:41:14 +0700

    Add test for fail sending message

--- wappin_test.go
+++ wappin_test.go
@@ -40,6 +40,40 @@ func TestSendNotificationHSM(t *testing.T) {
 	assert.Equal(t, "Success", res.Message)
 }
 
+func TestFailSendNotificationHSM(t *testing.T) {
+	httpmock.ActivateNonDefault(client.GetClient())
+	defer httpmock.DeactivateAndReset()
+	mockGetAccessToken()
+
+	fixture := `{ "message_id": "id-124", "status": "600", "message": "Not delivered, Contact validate Failed" }`
+	responder := httpmock.NewStringResponder(200, fixture)
+	fakeUrl := baseUrl + SendHsmEndpoint
+	httpmock.RegisterResponder("POST", fakeUrl, responder)
+
+	config := Config{
+		ProjectId:    "0123",
+		ClientSecret: "cs-key",
+		ClientKey:    "ck-key",
+	}
+	sender := New(config)
+	reqMsg := ReqWaMessage{
+		ClientId:        "123",
+		ProjectId:       "0123",
+		Type:            "template_name",
+		RecipientNumber: "089891234123",
+		Params: map[string]string{
+			"1": "John",
+			"2": "Depok",
+		},
+	}
+
+	res, _ := sender.SendMessage(reqMsg)
+
+	assert.Equal(t, "id-124", res.MessageId)
+	assert.Equal(t, "600", res.Status)
+	assert.Equal(t, "Not delivered, Contact validate Failed", res.Message)
+}
+
 func mockGetAccessToken() {
 	fixture := `{ "status": "200", "message": "Success", "data": { "access_token": "677b800f9b694f98bb9db6edb18336743a3f416cadff1953a59190f309220936", "expired_datetime": "2020-12-28 10:20:23", "token_type": "Bearer" } }`
 	responder := httpmock.NewStringResponder(200, fixture)
